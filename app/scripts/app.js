c5a5064cd462cb995ce900e18b5a2ad3b4c411bf
larismourullo <larismourullo@live.com>
2017-08-19 07:26:13 -0300

    :tada: Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appApp
+ * @description
+ * # appApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
